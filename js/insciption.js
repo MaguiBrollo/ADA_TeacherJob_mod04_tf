2145e6981bf52540057f0d58bfb45643f71f3ddd
Magui Brollo <maguieb@hotmail.com>
2024-04-19 19:49:07 -0300

    Inscripción: validaciónes en POST.

--- js/insciption.js
+++ js/insciption.js
@@ -10,51 +10,48 @@ const buscarRegional = (busco) => {
 	return reg.find((e) => e.cod === busco) ?? -1;
 };
 
-// ===================================================
-// Limpiar input
-function limpiarVolver() {
-	$("#cont-inscripcion").classList.add("ocultar");
-	$("#menu-aspirantes").classList.remove("ocultar");
-
-	$("#apellidos").value = "";
-	$("#nombres").value = "";
-	$("#sexo").value = "";
-	$("#telefono").value = "";
-	$("#correo").value = "";
-	$("#url-foto").value = "";
-	// $("#area").value = "";
-	// $("#regional").value = "";
-	$("#titulo").value = "";
-	$("#hs-dispo").value = "";
-	$("#puntaje").value = "";
+function quitaAcentos(frase) {
+	frase = frase.replace(/[áäâà]/g, "a");
+	frase = frase.replace(/[éëêè]/g, "e");
+	frase = frase.replace(/[íïîì]/g, "i");
+	frase = frase.replace(/[óöôò]/g, "o");
+	frase = frase.replace(/[úüûù]/g, "u");
+	return frase;
 }
 
 // ===================================================
 // Cancelar inscripción
 $("#btn-cancelar-insc").addEventListener("click", () => {
-	limpiarVolver();
+	$("#nueva-alumna-form").reset();
+	$("#cont-inscripcion").classList.add("ocultar");
+	$("#menu-aspirantes").classList.remove("ocultar");
 });
 
 // ============================================
 // Nueva Inscripción
-$("#btn-agregar-insc").addEventListener("click", () => {
+/* $("#btn-agregar-insc").addEventListener("click", () => { */
+$("#nueva-alumna-form").addEventListener("submit", (e) => {
+	e.preventDefault();
+
 	inscripcion = {
-		apellidos: $("#apellidos").value,
-		nombres: $("#nombres").value,
+		apellidos: quitaAcentos($("#apellidos").value).toUpperCase(),
+		nombres: quitaAcentos($("#nombres").value).toUpperCase(),
 		sexo: $("#sexo").value,
 		telefono: $("#telefono").value,
-		email: $("#correo").value,
+		email: quitaAcentos($("#correo").value).toUpperCase(),
 		foto_perfil: $("#url-foto").value,
 		area: $("#area").value,
 		regional: $("#regional").value,
-		titulo_area: $("#titulo").value,
+		titulo_area: quitaAcentos($("#titulo").value).toUpperCase(),
 		horas_dispo: $("#hs-dispo").value,
 		puntaje: $("#puntaje").value,
 	};
 
 	registrarInscripcion(inscripcion);
 
-	limpiarVolver();
+	$("#nueva-alumna-form").reset();
+	$("#cont-inscripcion").classList.add("ocultar");
+	$("#menu-aspirantes").classList.remove("ocultar");
 });
 
 // -------------------------
